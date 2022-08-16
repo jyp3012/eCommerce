0ebb192ae4aab4f29f7558cd5d5fc85091651b5e
pjy3012 <ppp0868@naver.com>
2022-08-16 15:27:18 +0900

    Initial commit

---
+++ src/test/java/com/zerobase/ecommerce_user/ECommerceUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.ecommerce_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
